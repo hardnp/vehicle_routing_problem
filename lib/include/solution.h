8ba8c2c35f59ad573aaf91510b71d4c974aacb06
andrgolubev <andpgolubev@gmail.com>
2018-11-23 00:06:10 +0300

    Implement objective function computation

--- lib/include/solution.h
+++ lib/include/solution.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#include "vehicle.h"
+
+#include <utility>
+#include <vector>
+
 namespace vrp {
 /// Solution representation
 class Solution {
-
+public:
+    using VehicleIndex = size_t;  ///< vehicle index in problem's vector
+                                  /// index != id
+    using CustomerIndex = size_t;  ///< customer index in problem's vector
+                                   /// index != id
+    std::vector<std::pair<VehicleIndex, std::vector<CustomerIndex>>> routes;
 };
 }  // vrp
--- lib/src/objective.cpp
+++ lib/src/objective.cpp
@@ -2,6 +2,18 @@
 
 namespace vrp {
 double objective(const Problem& prob, const Solution& sln) {
-    return 0.;
+    double A = 1.;  // TODO(agolubev): add to CSV
+    double objective_value = 0.;
+    for (const auto& route : sln.routes) {
+        const auto& vehicle = prob.vehicles[route.first];
+        const auto& customers = route.second;
+        for (size_t i = 0; i < customers.size() - 1; ++i) {
+            objective_value += vehicle.variable_cost
+                * prob.costs[customers[i]][customers[i+1]];
+            objective_value += A * prob.times[customers[i]][customers[i+1]];
+        }
+        objective_value += vehicle.fixed_cost;
+    }
+    return objective_value;
 }
 }  // vrp
