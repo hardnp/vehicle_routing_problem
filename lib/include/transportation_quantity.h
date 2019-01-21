24d63aa9a1cb2ddde9b5b06bff2a690387b1a7ea
Sean Kalejs <seankalejs@inbox.ru>
2019-01-21 21:44:35 +0300

    Transportation Quantity without vehicle comparison added (fixed)

--- lib/include/transportation_quantity.h
+++ lib/include/transportation_quantity.h
@@ -1,10 +1,6 @@
-#pragma once
-
-#include <cstdint>
-
 namespace vrp {
 /// Transportation Quantity representation
-class TrQuant {
+class TransportationQuantity {
 public:
     int volume = 0;
     int weight = 0;
--- lib/include/vehicle.h
+++ lib/include/vehicle.h
@@ -8,7 +8,7 @@ namespace vrp {
 class Vehicle {
 public:
     int id = 0;  ///< vehicle id
-    TrQuant trquant_v; ///vehicle quantity
+    TransportationQuantity capacity; ///vehicle quantity
     double fixed_cost = 0.;  ///< fixed vehicle cost
     double variable_cost = 0.;  ///< variable vehicle cost
 };
