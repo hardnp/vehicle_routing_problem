05fe96db083466f6d412ead74825486bdde24af8
M Kopysov <juggson@protonmail.ch>
2019-02-03 23:10:16 +0300

    Sequential Insertion heuristics

---
+++ lib/include/seq_insertion.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 1/13/19.
+//
+
+#ifndef VRP_SOLVER_SEQ_INSERTION_H
+#define VRP_SOLVER_SEQ_INSERTION_H
+
+#endif //VRP_SOLVER_SEQ_INSERTION_H
---
+++ lib/src/internal/seq_insertion.cpp
@@ -0,0 +1,4 @@
+//
+// Created by user on 1/13/19.
+//
+
