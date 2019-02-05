94a2ab95f587eb7d787a9cde2439405335e451db
andrgolubev <andpgolubev@gmail.com>
2019-02-05 23:38:12 +0300

    Add TransportationQuantity's comparison operators

--- lib/include/transportation_quantity.h
+++ lib/include/transportation_quantity.h
@@ -3,8 +3,43 @@
 namespace vrp {
 /// Transportation Quantity representation
 class TransportationQuantity {
+    inline bool incomparable(TransportationQuantity other) const {
+        return (volume < other.volume && weight > other.weight)
+            || (volume > other.volume && weight < other.weight)
+            || (volume == other.volume && weight != other.weight)
+            || (volume != other.volume && weight == other.weight);
+    }
 public:
     int volume = 0;
     int weight = 0;
+
+    inline bool operator<(TransportationQuantity other) const {
+        return volume < other.volume && weight < other.weight;
+    }
+
+    inline bool operator>(TransportationQuantity other) const {
+        return volume > other.volume && weight > other.weight;
+    }
+
+    inline bool operator<=(TransportationQuantity other) const {
+        return volume <= other.volume && weight <= other.weight;
+    }
+
+    inline bool operator>=(TransportationQuantity other) const {
+        return volume >= other.volume && weight >= other.weight;
+    }
+
+    // Note: The last ones are complicated. In some sense, == is equal to !=
+    // Why: if we can't really compare the objects (say whether one < another),
+    // those objects are incomparable and thus "equivalent" to us
+    inline bool operator!=(TransportationQuantity other) const {
+        return (volume != other.volume && weight != other.weight)
+            || incomparable(other);
+    }
+
+    inline bool operator==(TransportationQuantity other) const {
+        return (volume == other.volume && weight == other.weight)
+            || incomparable(other);
+    }
 };
 }  // vrp
