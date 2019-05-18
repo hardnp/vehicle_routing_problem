610cee3278b99a14b3b76925f418dacb470f5c40
Sean Kalejs <seankalejs@inbox.ru>
2019-05-18 20:07:04 +0300

    The first and second stages added

--- lib/src/internal/parallel_insertion.cpp
+++ lib/src/internal/parallel_insertion.cpp
@@ -31,8 +31,8 @@ namespace vrp {
             int the_first_customer = range(r_gen);
 
             //find the biggest vehicle
-            int *biggest_veh_pointer; /// try to set as auto below if wouldn't work
-            biggest_veh_pointer = std::max_element(prob.vehicles.begin(),
+            //int *biggest_veh_pointer; /// try to set as auto below if wouldn't work
+            auto biggest_veh_pointer = std::max_element(prob.vehicles.begin(),
                     prob.vehicles.end(), [](const Vehicle &a,
                             const Vehicle &b){
                 return a.capacity.volume < b.capacity.volume;
@@ -55,29 +55,31 @@ namespace vrp {
             i = 0;
             while(*biggest_veh_pointer < prob.customers[i].demand.volume){
                 if(max_splits > 0){
-                    splited_customers.push_back({prob.customers.id,
-                                                 prob.customers.demand.volume,
-                                                 prob.customers.demand.volume});
+                    splited_customers.push_back({prob.customers[i].id,
+                                                 prob.customers[i].demand.volume,
+                                                 prob.customers[i].demand.volume
+                    });
                     max_splits--;
                     i++;
                 }
                 else{
-                    cout << "No Solution (not enough splits)" << endl;
+                    std::cout << "No Solution (not enough splits)" << std::endl;
                     return 0;
                 }
             }
 
 
             // sort customers by remoteness from depo
-            // TODO!
-            std::sort(prob.customers.begin(), prob.customers.end(),
-                      [](const Customer &a, const Customer &b){
-                          return a.demand.volume < b.demand.volume;
+            std::sort(prob.times[0].begin(), prob.times[0].end(),
+                    greater<int>());
+
+
+            // sort vehicles by fixed_cost/volume
+            std::sort(prob.vehicles.begin(), prob.vehicles.end(),
+                      [](const Vehicle &a, const Vehicle &b){
+                          return a.fixed_cost/a.capacity.volume <
+                          b.fixed_cost/b.capacity.volume;
                       });
-
-
-
-
 
 
 
